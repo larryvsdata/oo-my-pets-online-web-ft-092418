103566f79c7f63e58b921c8b51a6950d21bc70a9
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-05 15:08:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,6 +57,12 @@ class Owner
       def play_with_cats
        self.pets[:cats] = self.pets[:cats].map{ |cat| cat.mood="happy"  }
      end
+     
+      def play_with_cats
+       self.pets[:cats] = self.pets[:cats].map{ |cat| cat.mood="happy"  }
+     end
+     
+     
   
   
 end
