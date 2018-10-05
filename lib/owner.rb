68ec929508fdefbce568dbcca61a7c6fa4b0fb96
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-05 13:34:53 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
-  # code goes here
+  attr_reader :name , :species
+  @@all=[]
+  @@count = 0
+  
+    def initialize(name)
+    @name = name
+    @@all << self
+    @@count += 1
+    @species = "human"
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.reset_all
+    @@all.clear
+    @@count = 0
+  end
+  
 end
